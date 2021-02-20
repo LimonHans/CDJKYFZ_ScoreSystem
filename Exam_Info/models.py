0f83442fe0244fa1936190d71b9be0a51392cc1b
Limon <LimonHans@users.noreply.github.com>
2021-02-20 21:55:37 +0800

    Test-1
    Co-Authored-By: dixiatielu <dxtl666@foxmail.com>

--- Exam_Info/models.py
+++ Exam_Info/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class PostExam(models.Model):
+    title = 
+
+    def __str__(self):
+        return self.title
