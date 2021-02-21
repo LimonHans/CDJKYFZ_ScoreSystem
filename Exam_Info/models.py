d2762ae35c5e26e5ed584a161720f212aeb18f73
Limon <LimonHans@users.noreply.github.com>
2021-02-21 14:56:54 +0800

    Echarts boxplot added

--- Exam_Info/models.py
+++ Exam_Info/models.py
@@ -16,8 +16,12 @@ from django.db import models
 class Exam(models.Model):
     # 考试标题
     title = models.CharField(blank = False, max_length = 200, help_text = "考试标题", default = "未指定标题")
-    
     description = models.TextField(blank = True, help_text = "考试描述", default = "无描述")
+    value_min = models.IntegerField(blank = False, help_text = "Min Scores", default = 0)
+    value_Q1 = models.IntegerField(blank = False, help_text = "Q1 Scores", default = 0)
+    value_Q2 = models.IntegerField(blank = False, help_text = "Q2 Scores", default = 75)
+    value_Q3 = models.IntegerField(blank = False, help_text = "Q3 Scores", default = 150)
+    value_max = models.IntegerField(blank = False, help_text = "Max Scores", default = 150)
 
     # 考试时间
     time = models.DateField()
