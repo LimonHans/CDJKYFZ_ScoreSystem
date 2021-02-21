3888bc27d094a21dec70a8e7c3cb7ff7c1fe6b0a
dixiatielu <1397749594@qq.com>
2021-02-21 14:57:06 +0800

    models

--- Exam_Info/models.py
+++ Exam_Info/models.py
@@ -15,20 +15,38 @@ from django.db import models
 # 考试类
 class Exam(models.Model):
     # 考试标题
-    title = models.CharField(blank = False, max_length = 200, help_text = "考试标题", default = "未指定标题")
-    
-    description = models.TextField(blank = True, help_text = "考试描述", default = "无描述")
-
+    title = models.CharField(verbose_name = "标题", blank = False, max_length = 200, help_text = "考试标题", default = "未指定标题")
+    # 考试描述
+    description = models.TextField(verbose_name = "描述", blank = True, help_text = "考试描述", default = "暂无描述")
     # 考试时间
     time = models.DateField()
+    # 年级元组
+    GRADES = (
+        (1, '高一'),
+        (2, '高二'),
+        (3, '高三')
+    )
+    # 考试年级
+    grade = models.PositiveSmallIntegerField(verbose_name = "考试年级", blank = False, help_text = "请填入1或2或3，表示高一或高二或高三。", choices = GRADES)
+    # 参加考试的学生
+    students = models.ManyToManyField('Student')
 
     def __str__(self):
         return self.title
-#This class should appear in another app.
-## 学生类
-#class Student(models.Model):
-#    # 姓名
-#    name = models.CharField(null = False, blank = False, max_length = 10, help_text = "学生姓名", default = "无名氏")
-#
-#    def __str__(self):
-#        return self.name
+
+# This class shouldn't appear in another app. :D
+# 学生类
+class Student(models.Model):
+    # 姓名
+    name = models.CharField(verbose_name = "姓名", blank = False, max_length = 10, help_text = "学生姓名", default = "无名氏")
+    # 性别元组
+    GENDERS = (
+        ('M', '男'),
+        ('F', '女'),
+        ('N', '保密')
+    )
+    # 性别
+    gender = models.CharField(verbose_name = "性别", blank = False, max_length = 1, help_text = "性别", choices = GENDERS)
+
+    def __str__(self):
+        return self.name
