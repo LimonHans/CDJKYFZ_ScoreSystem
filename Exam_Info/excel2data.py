80e287ec4bcea378c8a88d3b264241022cf96eb4
Limon <LimonHans@users.noreply.github.com>
2021-02-23 19:15:14 +0800

    Counter for excel2data.py

--- Exam_Info/excel2data.py
+++ Exam_Info/excel2data.py
@@ -27,3 +27,24 @@ Score_WL = data[:, 10]# 物理得分 -- 10
 Score_HX = data[:, 11]# 化学得分 -- 11
 Score_SW = data[:, 12]# 生物得分 -- 12
 #Finder = dict()# map
+
+#'G' names for 'Goal'
+#Gscore_Overall = []# 总成绩分段，每一个段占两个空
+# 这个功能合并至具体的展示界面
+
+Gstudent_Numall = len(Rank_Overall)# 参考的所有人数
+Gstudent_NumActual = Gstudent_Numall# 有效参考人数(任何一个科目都不为零)
+while (not(Score_YW[Gstudent_NumActual - 1] != 0 and Score_SX[Gstudent_NumActual - 1] != 0 and Score_WY[Gstudent_NumActual - 1] != 0 and Score_WL[Gstudent_NumActual - 1] != 0 and Score_HX[Gstudent_NumActual - 1] != 0 and Score_SW[Gstudent_NumActual - 1] != 0)):
+    Gstudent_NumActual -= 1
+print(Gstudent_Numall)
+print(Gstudent_NumActual)
+
+GQ1 = []
+GQ2 = [sum(Score_Overall)/2, sum(Score_YW)/2, sum(Score_SX)/2, sum(Score_WY)/2, sum(Score_WL)/2, sum(Score_HX)/2, sum(Score_SW)/2]
+GQ3 = []
+for i in range(0, 7):
+    GQ1.append(GQ2[i]/2)
+    GQ3.append(GQ1[i]*3)
+print(GQ1)
+print(GQ2)
+print(GQ3)
