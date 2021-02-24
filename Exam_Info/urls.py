7f2a4ef4db3f1aa3e0c21dee99b077e4aa7c026d
dixiatielu <1397749594@qq.com>
2021-02-24 22:08:43 +0800

    [A] ContrastStu

--- Exam_Info/urls.py
+++ Exam_Info/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name = 'index'),
-    path('exams/<int:pk>/exam_details/', views.DetailView.as_view(), name = 'exam_details')
+    path('exams/<int:pk>/exam_details/', views.DetailView.as_view(), name = 'exam_details'),
+    re_path(r'^students_contrast/$', views.ContrastStudentsView, name = 'students_contrast') # 多学生对比分析路由
 ]
