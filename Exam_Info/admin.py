6cc7e53c9db530470df8e6cdce44beeace700ee7
dixiatielu <1397749594@qq.com>
2021-02-24 11:26:44 +0800

    [A] Model ExamScore

--- Exam_Info/admin.py
+++ Exam_Info/admin.py
@@ -2,23 +2,52 @@ from django.contrib import admin
 from .models import *
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+from import_export.fields import Field
+from import_export.widgets import ForeignKeyWidget
 # Register your models here.
 
 # Plugin: Import Export
 class StudentResource(resources.ModelResource):
+    name = Field(attribute = 'name', column_name = '姓名')
+    pk = Field(attribute = 'id', column_name = '考号')
+    gender = Field(attribute = 'gender', column_name = '性别')
+    in_class = Field(attribute = 'in_class', column_name = '班级', widget = ForeignKeyWidget(Class))
+
     class Meta:
         model = Student
         export_order = ('id', 'name', 'gender')
+
+class ExamScoreResource(resources.ModelResource):
+    grade_rank = Field(attribute = 'grade_rank', column_name = '年级排名')
+    class_rank = Field(attribute = 'class_rank', column_name = '班级排名')
+    
+    yw = Field(attribute = 'yw', column_name = '语文')
+    sx = Field(attribute = 'sx', column_name = '数学')
+    yy = Field(attribute = 'yy', column_name = '英语')
+    wl = Field(attribute = 'wl', column_name = '物理')
+    hx = Field(attribute = 'hx', column_name = '化学')
+    sw = Field(attribute = 'sw', column_name = '生物')
+    total_score = Field(attribute = 'total_score', column_name = '总分')
+
+    student = Field(attribute = 'student', column_name = '考号', widget = ForeignKeyWidget(Student))
+    # exam = Field(attribute = 'exam', column_name = '考试标题', default = )
+    class Meta:
+        model = ExamScore
+        fields = ('id', 'student', 'class_rank', 'total_score', 'yw', 'sx', 'yy', 'wl', 'hx', 'sw')
 
 # Admin for models
 @admin.register(Exam)
 class ExamAdmin(ImportExportModelAdmin):
     pass
 
-@admin.register(Student)
-class StudentAdmin(ImportExportModelAdmin):
-    pass
-
 @admin.register(Class)
 class ClassAdmin(ImportExportModelAdmin):
     pass
+
+@admin.register(Student)
+class StudentAdmin(ImportExportModelAdmin):
+    resource_class = StudentResource
+
+@admin.register(ExamScore)
+class ExamScoreAdmin(ImportExportModelAdmin):
+    resource_class = ExamScoreResource
--- Exam_Info/models.py
+++ Exam_Info/models.py
@@ -8,6 +8,10 @@ from django.db import models
 class Class(models.Model):
     name = models.CharField(verbose_name = "班级名称", max_length = 100)
 
+    class Meta:
+        verbose_name = '班级'
+        verbose_name_plural = verbose_name
+
     def __str__(self):
         return self.name
 
@@ -35,6 +39,10 @@ class Exam(models.Model):
     # 参加考试的学生
     students = models.ManyToManyField('Student', default = "")
 
+    class Meta:
+        verbose_name = '考试'
+        verbose_name_plural = verbose_name
+
     def __str__(self):
         return self.title
 
@@ -48,14 +56,33 @@ class Student(models.Model):
         ('F', '女'),
         ('N', '保密')
     )
-    gender = models.CharField(verbose_name = "性别", max_length = 1, help_text = "性别", choices = GENDERS)
+    gender = models.CharField(verbose_name = "性别", max_length = 1, help_text = "性别", choices = GENDERS, blank = True)
     enter_date = models.DateField(verbose_name = "入学时间", default = timezone.now)
-    ######################################
-    # 这个肯定要改吧，啥时候注册啥时候入学吗... #
-    ######################################
     remarks = models.TextField(verbose_name = "备注", blank = True)
     in_class = models.ForeignKey(to = Class, verbose_name = "所在班级", on_delete=models.CASCADE, null = True)
+
+    class Meta:
+        verbose_name = '学生'
+        verbose_name_plural = verbose_name
 
     def __str__(self):
         return self.name
 
+class ExamScore(models.Model):
+    student = models.ForeignKey(to = Student, verbose_name = "姓名", on_delete=models.CASCADE)
+    exam = models.ForeignKey(to = Exam, verbose_name = "对应考试", on_delete=models.CASCADE)
+
+    yw = models.DecimalField(max_digits = 5, decimal_places = 2, verbose_name = "语文成绩")
+    sx = models.DecimalField(max_digits = 5, decimal_places = 2, verbose_name = "数学成绩")
+    yy = models.DecimalField(max_digits = 5, decimal_places = 2, verbose_name = "英语成绩")
+    wl = models.DecimalField(max_digits = 5, decimal_places = 2, verbose_name = "物理成绩")
+    hx = models.DecimalField(max_digits = 5, decimal_places = 2, verbose_name = "化学成绩")
+    sw = models.DecimalField(max_digits = 5, decimal_places = 2, verbose_name = "生物成绩")
+    total_score = models.DecimalField(max_digits = 5, decimal_places = 2, verbose_name = "总分")
+
+    grade_rank = models.IntegerField()
+    class_rank = models.IntegerField()
+
+    class Meta:
+        verbose_name = '考试信息'
+        verbose_name_plural = verbose_name
