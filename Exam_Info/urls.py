c8c79870bde577d8744232b5a6b56f5defbbb32f
Limon <LimonHans@users.noreply.github.com>
2021-02-25 15:20:25 +0800

    Update ClassAll.html

--- Exam_Info/urls.py
+++ Exam_Info/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name = 'index'),
     path('exam/<int:pk>/exam_details/', views.ExamDetailView.as_view(), name = 'exam_details'),
-    path('class/all/', views.ClassOverView.as_view(), name = 'class_all'),
+    path('class/', views.ClassOverView.as_view(), name = 'class_all'),
     path('class/<int:pk>/class_details/', views.ClassDetailView.as_view(), name = 'class_details')
 #    re_path(r'^students_contrast/$', views.ContrastStudentsView, name = 'students_contrast') # 多学生对比分析路由
 ]
