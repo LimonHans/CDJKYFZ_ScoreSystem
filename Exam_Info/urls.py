71ad6f7563608da9febb891c58908aa15a4e3ff8
Limon <LimonHans@users.noreply.github.com>
2021-02-25 23:16:25 +0800

    requirement update-3

--- Exam_Info/urls.py
+++ Exam_Info/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     # TODO
     # 1. 需要传递的内容包括：(注意有前缀：yw.,sx.,yy.,wl.,hx.,sw.)
     #    .name(用以描述本次考试，记录为字符串，格式为“考试<...>的<...>科目”), .value_min, .value_max, .value_Q1, .value_Q2, .value_Q3,
-    #    .value_scoremax(用以表示本次考试该科目理论最高分)
+    #    .value_scoremax(用以表示本次考试该科目理论最高分);
+    #    但是在 exam_details 下的页面时，将不要前缀，如在 exam 时的 yw.name 相当于在 该页面下的 name
     # 2. 同时还有Value_Bar10Score[], Value_Bar20Rank[]分别用来描述 从理论最高分到理论最低分的每十分分段、从理论第一名到理论最末名的每20人分段
     # 3. 对于总分，同样也需要 1. 中的内容，前缀：zf
     path('class/', views.ClassOverView.as_view(), name = 'class_all'),
