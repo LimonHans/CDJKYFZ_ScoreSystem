2fcc935fc9b324990d531ecd93e6667ee97da246
Limon <LimonHans@users.noreply.github.com>
2021-02-25 23:09:01 +0800

    requirement update-2 && other things

--- Exam_Info/urls.py
+++ Exam_Info/urls.py
@@ -6,10 +6,12 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name = 'index'),
     path('exam/', views.ExamOverView.as_view(), name = 'exam_all'),
     path('exam/exam_details/<str:exam_title>/', views.ExamDetailView.as_view(), name = 'exam_details'),
-    # 1. 需要传递的内容包括：
-    #    title(用以描述本次考试，记录为字符串，格式为“考试<...>的<...>科目”), Value_min, Value_max, Value_Q1, Value_Q2, Value_Q3,
-    #    Value_ScoreMax(用以表示本次考试该科目理论最高分)
-    # 2. 同时还有Value_Bar10Score, Value_Bar10
+    # TODO
+    # 1. 需要传递的内容包括：(注意有前缀：yw.,sx.,yy.,wl.,hx.,sw.)
+    #    .name(用以描述本次考试，记录为字符串，格式为“考试<...>的<...>科目”), .value_min, .value_max, .value_Q1, .value_Q2, .value_Q3,
+    #    .value_scoremax(用以表示本次考试该科目理论最高分)
+    # 2. 同时还有Value_Bar10Score[], Value_Bar20Rank[]分别用来描述 从理论最高分到理论最低分的每十分分段、从理论第一名到理论最末名的每20人分段
+    # 3. 对于总分，同样也需要 1. 中的内容，前缀：zf
     path('class/', views.ClassOverView.as_view(), name = 'class_all'),
     path('class/<int:pk>/class_details/', views.ClassDetailView.as_view(), name = 'class_details')
 #    re_path(r'^students_contrast/$', views.ContrastStudentsView, name = 'students_contrast') # 多学生对比分析路由
