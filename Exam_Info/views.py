d04d23bc1afc2cf60667db49dd1bb538a073c6fe
Limon <LimonHans@users.noreply.github.com>
2021-02-25 14:42:17 +0800

    Add class overview.

--- Exam_Info/views.py
+++ Exam_Info/views.py
@@ -14,13 +14,29 @@ class IndexView(generic.ListView):
             Exam.objects.order_by('-time')[0:2]
         }
 
-class DetailView(generic.DetailView):
+class ExamDetailView(generic.DetailView):
     template_name = 'Exam_Info/ExamDetails.html'
     model = Exam
     context_object_name = 'NowExam'
 
     def get_queryset(self):
         return Exam.objects.order_by('-time')
+
+class ClassOverView(generic.ListView):
+    template_name = 'Exam_Info/ClassAll.html'
+    model = Class
+    context_object_name = 'ClassList'
+
+    def get_queryset(self):
+        return Class.objects.order_by('-id')
+
+class ClassDetailView(generic.DetailView):
+    template_name ='Exam_Info/ClassDetails.html'
+    model = Class
+    context_object_name = 'NowClass'
+
+    def get_queryset(self):
+        return Class.objects.order_by('-time')
 
 #def ContrastStudentsView(request):
 #    # TODO
