14e8d77bec3d511dea7ca176e8b102ed7b1545d6
Steve D <1397749594@qq.com>
2021-03-07 10:38:46 +0800

    [F]NoneType queryset

--- Exam_Info/views.py
+++ Exam_Info/views.py
@@ -36,44 +36,46 @@ class ExamDetailView(generic.DetailView):
     context_object_name = 'NowExam'
 
     def get_queryset(self):
-        return ExamScore.objects.all()
-        # return ExamScore.objects.filter(exam__id = self.kwargs['pk']).order_by('-time')
+        # return ExamScore.objects.all()
+        return ExamScore.objects.filter(exam__id = self.kwargs['pk'])
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context.update(self.queryset.aggregate(value_max = Max('total_score'))) # 为上下文加入总分最大值
-        context.update(self.queryset.aggregate(value_min = Min('total_score'))) # 为上下文加入总分最小值
-        context['value_Q1'], context['value_Q2'], context['value_Q3'] = np.percentile(list(self.queryset.values('total_score')), [25, 50, 75]) # 同理，总分的四分
+        query_set = self.get_queryset()
+        context.update(query_set.aggregate(value_max = Max('total_score'))) # 为上下文加入总分最大值
+        context.update(query_set.aggregate(value_min = Min('total_score'))) # 为上下文加入总分最小值
+        # context['value_Q1'], context['value_Q2'], context['value_Q3'] = np.percentile(list(query_set.values('total_score')), [25, 50, 75]) # 同理，总分的四分
 
         # 语文
-        context.update(self.queryset.aggregate(value_yw_max = Max('yw'))) # 为上下文加入最大值
-        context.update(self.queryset.aggregate(value_yw_min = Min('yw'))) # 为上下文加入最小值
+        context.update(query_set.aggregate(value_yw_max = Max('yw'))) # 为上下文加入最大值
+        context.update(query_set.aggregate(value_yw_min = Min('yw'))) # 为上下文加入最小值
 
         # 数学
-        context.update(self.queryset.aggregate(value_sx_max = Max('sx')))
-        context.update(self.queryset.aggregate(value_sx_min = Min('sx')))
+        context.update(query_set.aggregate(value_sx_max = Max('sx')))
+        context.update(query_set.aggregate(value_sx_min = Min('sx')))
 
         # 英语
-        context.update(self.queryset.aggregate(value_yy_max = Max('yy')))
-        context.update(self.queryset.aggregate(value_yy_min = Min('yy')))
+        context.update(query_set.aggregate(value_yy_max = Max('yy')))
+        context.update(query_set.aggregate(value_yy_min = Min('yy')))
 
         # 物理
-        context.update(self.queryset.aggregate(value_wl_max = Max('wl')))
-        context.update(self.queryset.aggregate(value_wl_min = Min('wl')))
+        context.update(query_set.aggregate(value_wl_max = Max('wl')))
+        context.update(query_set.aggregate(value_wl_min = Min('wl')))
 
         # 化学
-        context.update(self.queryset.aggregate(value_hx_max = Max('hx')))
-        context.update(self.queryset.aggregate(value_hx_min = Min('hx')))
+        context.update(query_set.aggregate(value_hx_max = Max('hx')))
+        context.update(query_set.aggregate(value_hx_min = Min('hx')))
 
         # 生物
-        context.update(self.queryset.aggregate(value_sw_max = Max('sw')))
-        context.update(self.queryset.aggregate(value_sw_min = Min('sw')))
+        context.update(query_set.aggregate(value_sw_max = Max('sw')))
+        context.update(query_set.aggregate(value_sw_min = Min('sw')))
 
         subjects = ('yw', 'sx', 'yy', 'wl', 'hx', 'sw')
 
         for subject in subjects:
-            context[f'value_{subject}_Q1'], context[f'value_{subject}_Q2'], context[f'value_{subject}_Q3'] = np.percentile(list(self.queryset.values(subject)), [25, 50, 75]) # 同理，各个学科的四分
+            pass
+            # context[f'value_{subject}_Q1'], context[f'value_{subject}_Q2'], context[f'value_{subject}_Q3'] = np.percentile(list(query_set.values(subject)), [25, 50, 75]) # 同理，各个学科的四分
         
         return context
 
